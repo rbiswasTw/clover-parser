c4f2547743233cc1e32479a7cd7e2516a01a8ba3
rbiswasTw <rudrajyoti.biswas@thoughtworks.com>
2023-09-28 08:35:27 +0530

    zip with workspace. workspace in present directory is default location

--- src/main/java/com/tbc/enablement/clover/report/Main.java
+++ src/main/java/com/tbc/enablement/clover/report/Main.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
         ReportGenerator reportGenerator = new ReportGenerator();
-        reportGenerator.generateComprehensiveReport(args[0]);
+        String workspaceDir = args.length > 0 ? args[0] : String.format("%s/%s", System.getProperty("user.dir"),"workspace");
+        reportGenerator.generateComprehensiveReport(workspaceDir);
     }
 }
